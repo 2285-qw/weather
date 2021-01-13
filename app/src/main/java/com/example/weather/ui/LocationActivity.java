cd4ab92a69aee107f6008d1cb4304b8bdb622920
2285-qw <2285094427@qq.com>
2021-01-13 18:17:40 +0800

    1-13

---
+++ app/src/main/java/com/example/weather/ui/LocationActivity.java
@@ -0,0 +1,246 @@
+package com.example.weather.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.example.weather.R;
+import com.example.weather.application.BaseApplication;
+import com.example.weather.service.LocationService;
+import com.example.weather.service.Utils;
+
+public class LocationActivity extends AppCompatActivity {
+    private LocationService locationService;
+    private TextView LocationResult;
+    private TextView LocationDiagnostic;
+    private Button startLocation;
+    private  String location1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        // -----------demo view config ------------
+        setContentView(R.layout.activity_location);
+        LocationResult = (TextView) findViewById(R.id.textView1);
+        LocationDiagnostic = (TextView) findViewById(R.id.textView2);
+        LocationResult.setMovementMethod(ScrollingMovementMethod.getInstance());
+        startLocation = (Button) findViewById(R.id.addfence);
+    }
+
+    /**
+     * 显示请求字符串
+     *
+     * @param str
+     */
+    public void logMsg(final String str, final int tag) {
+
+        try {
+            if (LocationResult != null) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LocationResult.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (tag == Utils.RECEIVE_TAG) {
+                                    LocationResult.setText(str);
+                                    Log.d("tag",str);
+                                } else if (tag == Utils.DIAGNOSTIC_TAG) {
+                                    //LocationDiagnostic.setText(str);
+                                    Toast.makeText(LocationActivity.this, "str", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * Stop location service
+     */
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        locationService.unregisterListener(mListener); //注销掉监听
+        locationService.stop(); //停止定位服务
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        // -----------location config ------------
+        locationService = ((BaseApplication) getApplication()).locationService;
+        //获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
+        locationService.registerListener(mListener);
+        //注册监听
+        int type = getIntent().getIntExtra("from", 0);
+        if (type == 0) {
+            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
+        } else if (type == 1) {
+            locationService.start();
+        }
+        startLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationService.start();// 定位SDK
+                if (!location1.isEmpty()){
+                    startLocation.setText(location1);
+                }else{
+                    Toast.makeText(LocationActivity.this, "定位失败", Toast.LENGTH_SHORT).show();
+                }
+//                if (startLocation.getText().toString().equals(getString(R.string.startlocation))) {
+//                    locationService.start();// 定位SDK
+//                    // start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
+//                    startLocation.setText(getString(R.string.stoplocation));
+//                } else {
+//                    locationService.stop();
+//                    startLocation.setText(getString(R.string.startlocation));
+//                }
+            }
+        });
+    }
+
+
+    /*****
+     *
+     * 定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
+     *
+     */
+    private BDAbstractLocationListener mListener = new BDAbstractLocationListener() {
+
+        /**
+         * 定位请求回调函数
+         * @param location 定位结果
+         */
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            // TODO Auto-generated method stub
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                int tag = 1;
+                StringBuffer sb = new StringBuffer(256);
+                sb.append("time : ");
+                /**
+                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间
+                 *                  * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变；
+                 */
+                location1=location.getProvince();
+                sb.append(location.getTime());
+                sb.append("\nProvince : ");// 获取省份
+                sb.append(location.getProvince());
+                sb.append("\nCountry : ");// 国家名称
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");// 城市编码
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");// 城市
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");// 区
+                sb.append(location.getDistrict());
+
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                        sb.append("\nheight : ");
+                        sb.append(location.getAltitude());// 单位：米
+                    }
+                    sb.append("\noperationers : ");// 运营商信息
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+                logMsg(sb.toString(), tag);
+            }
+        }
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+            super.onConnectHotSpotMessage(s, i);
+        }
+
+        /**
+         * 回调定位诊断信息，开发者可以根据相关信息解决定位遇到的一些问题
+         * @param locType 当前定位类型
+         * @param diagnosticType 诊断类型（1~9）
+         * @param diagnosticMessage 具体的诊断信息释义
+         */
+        @Override
+        public void onLocDiagnosticMessage(int locType, int diagnosticType, String diagnosticMessage) {
+            super.onLocDiagnosticMessage(locType, diagnosticType, diagnosticMessage);
+            int tag = 2;
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("诊断结果: ");
+            if (locType == BDLocation.TypeNetWorkLocation) {
+                if (diagnosticType == 1) {
+                    sb.append("网络定位成功，没有开启GPS，建议打开GPS会更好");
+                    sb.append("\n" + diagnosticMessage);
+                } else if (diagnosticType == 2) {
+                    sb.append("网络定位成功，没有开启Wi-Fi，建议打开Wi-Fi会更好");
+                    sb.append("\n" + diagnosticMessage);
+                }
+            } else if (locType == BDLocation.TypeOffLineLocationFail) {
+                if (diagnosticType == 3) {
+                    sb.append("定位失败，请您检查您的网络状态");
+                    sb.append("\n" + diagnosticMessage);
+                }
+            } else if (locType == BDLocation.TypeCriteriaException) {
+                if (diagnosticType == 4) {
+                    sb.append("定位失败，无法获取任何有效定位依据");
+                    sb.append("\n" + diagnosticMessage);
+                } else if (diagnosticType == 5) {
+                    sb.append("定位失败，无法获取有效定位依据，请检查运营商网络或者Wi-Fi网络是否正常开启，尝试重新请求定位");
+                    sb.append(diagnosticMessage);
+                } else if (diagnosticType == 6) {
+                    sb.append("定位失败，无法获取有效定位依据，请尝试插入一张sim卡或打开Wi-Fi重试");
+                    sb.append("\n" + diagnosticMessage);
+                } else if (diagnosticType == 7) {
+                    sb.append("定位失败，飞行模式下无法获取有效定位依据，请关闭飞行模式重试");
+                    sb.append("\n" + diagnosticMessage);
+                } else if (diagnosticType == 9) {
+                    sb.append("定位失败，无法获取任何有效定位依据");
+                    sb.append("\n" + diagnosticMessage);
+                }
+            } else if (locType == BDLocation.TypeServerError) {
+                if (diagnosticType == 8) {
+                    sb.append("定位失败，请确认您定位的开关打开状态，是否赋予APP定位权限");
+                    sb.append("\n" + diagnosticMessage);
+                }
+            }
+            logMsg(sb.toString(), tag);
+        }
+    };
+
+}
